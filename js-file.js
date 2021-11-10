1dc28c98502767ad6979a734c26e5abff372b863
Torres2235 <Torres2235@gmail.com>
2021-11-10 14:04:57 -0800

    created table using JS

--- js-file.js
+++ js-file.js
@@ -1,3 +1,5 @@
+let container = document.getElementById('board');
+
 const playerFactory = (name, marker) => { //factory
     name = name;
     marker = marker;
@@ -7,8 +9,23 @@ const playerFactory = (name, marker) => { //factory
 
 const gameBoard = (() => { //module
     let _board = new Array(9);
+    
+    const drawGameBoard = () => {
+        for(let i = 0; i < 9; i++) {
+            let cell = document.createElement('div');
+            _board.push(cell)
+            cell.classList.toggle('square');
+            cell.id = i;
+            cell.textContent = "X";
+            container.appendChild(cell);
+        }
+    }
+    //console.table(_board);
+    return {drawGameBoard};
 })();
 
 const displayController = (() => { //module
     //stuff
 })();
+
+gameBoard.drawGameBoard();
